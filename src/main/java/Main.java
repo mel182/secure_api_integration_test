6039ed3f246aa2ec07fb697ed6bbfac3fddf0877
mel182 <42454027+mel182@users.noreply.github.com>
2019-04-25 17:29:53 +0200

    Code clean up and main java class cleaned up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,4 @@
-public class Main {
-
-    public static void main(String[] args)
-    {
-        System.out.println("Test main method");
-
-        System.out.println("Version 1.1");
-    }
-
-
+public class Main
+{
+    public static void main(String[] args) { }
 }
